4c6d0b6cb8dce64deaafec8653d12f43efa8d578
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-10-26 15:29:01 -0500

    Añade tabla encargada de almacenar la informacion contable usada para la conciliacion

---
+++ 05 - CUP_ConciliacionAuxiliarContable.sql
@@ -0,0 +1,56 @@
+SET ANSI_NULLS, ANSI_WARNINGS ON;
+
+IF OBJECT_ID('dbo.CUP_ConciliacionAuxiliarContable', 'U') IS NOT NULL 
+  DROP TABLE dbo.CUP_ConciliacionAuxiliarContable; 
+
+GO
+
+-- =============================================
+-- Created by:    Enrique Sierra Gtez
+-- Creation Date: 2016-10-26
+--
+-- Description: Tabla encargada de contener
+-- los auxiliares contables para las conciliaciones 
+-- auxiliar - cont.
+-- =============================================
+
+CREATE TABLE dbo.CUP_ConciliacionAuxiliarContable
+(
+  Empleado INT NOT NULL,
+  ID INT NOT NULL,
+  Cuenta     CHAR(20) NOT NULL,
+  Descripcion VARCHAR(100) NULL ,
+  CentroCostos VARCHAR(50) NULL,
+  Debe DECIMAL(18,4) NOT NULL,
+  Haber DECIMAL(18,4) NOT NULL,
+  Neto DECIMAL(18,4) NOT NULL,
+  Sucursal INT NULL,
+  FechaContable DATE NOT NULL,
+  Mov CHAR(20) NOT NULL,
+  MovId VARCHAR(20) NULL,
+  Referencia VARCHAR(50) NULL,
+  OrigenModulo CHAR(5) NULL,
+  OrigenModuloID INT NULL,
+  OrigenMov CHAR(20) NULL,
+  OrigenMovId VARCHAR(20) NULL,
+  AuxiliarModulo CHAR(5) NULL,
+  AuxiliarMov VARCHAR(20) NULL,
+  PRIMARY KEY ( 
+                Empleado,
+                ID,
+                Cuenta
+              )
+) 
+
+
+CREATE NONCLUSTERED INDEX [IX_CUP_CUP_ConciliacionAuxiliarContable_Empleado_AuxiliarModulo_AuxiliarMov]
+ON [dbo].[CUP_ConciliacionAuxiliarContable] ( Empleado, AuxiliarModulo, AuxiliarMov )
+INCLUDE ( 
+           Debe,
+           Haber,
+           Neto,
+           OrigenModulo,
+           OrigenModuloID,
+           OrigenMov,
+           OrigenMovId
+        )
