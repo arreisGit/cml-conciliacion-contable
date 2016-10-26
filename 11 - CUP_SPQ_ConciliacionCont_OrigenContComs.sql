153ede7615de30e52d7fdc88098db7806619e318
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-10-26 18:06:56 -0500

    Adapta el procedimiento encargado de obtener los origenes contables de compras, para devolver el resultado con los valores necesarios en la integracion de todo el auxiliar de modulos. Como parte de los preparativos para poner la caratula en el portal

--- 11 - CUP_SPQ_ConciliacionCont_OrigenContComs.sql
+++ 11 - CUP_SPQ_ConciliacionCont_OrigenContComs.sql
@@ -21,21 +21,17 @@ GO
 -- suficiente iformacion para poderlos cruzar 
 -- "lado a lado" con su póliza  contable.
 -- 
--- Example: EXEC CUP_SPQ_ConciliacionCont_OrigenContComs 2016, 9
+-- Example: EXEC CUP_SPQ_ConciliacionCont_OrigenContComs 63527, 1, 2016, 9
 -- =============================================
 
 
 CREATE PROCEDURE dbo.CUP_SPQ_ConciliacionCont_OrigenContComs
+  @Empleado  INT,
+  @Tipo      INT,
   @Ejercicio INT,
-  @Periodo INT
+  @Periodo   INT
 AS BEGIN 
 
-  DECLARE
-    @FechaInicio DATE = CAST(CAST(@Ejercicio AS VARCHAR)
-                                  + '-' 
-                                  + CAST(@Periodo AS VARCHAR)
-                                  + '-01' AS DATE)
-  
   -- Tabla utilizada a modo de "workaround" 
   -- para poder simular el efecto de "EsCancelacion"
   -- directo en el modulo.
@@ -49,100 +45,105 @@ AS BEGIN
                   EsCancelacion
                 )
   )
-  INSERT INTO @EstatusValidos
-  ( 
-    Estatus,
-    EsCancelacion,
-    Factor 
-  )
-  VALUES 
-    ( 'CONCLUIDO', 0,  1 ),
-    ( 'PENDIENTE', 0,  1 ),
-    ( 'CANCELADO', 0,  1 ), 
-    ( 'CANCELADO', 1, -1 )
 
-  SELECT
-    origenCont.Modulo,
-    m.ID,
-    m.Mov,
-    m.MovId,
-    m.Sucursal,
-    m.FechaEmision,
-    m.Proveedor,
-    ProvNombre =  REPLACE(REPLACE(REPLACE(Prov.Nombre,CHAR(13),''),CHAR(10),''),CHAR(9),''),
-    ProvCuenta =  Prov.Cuenta,
-    m.Estatus,
-    eV.EsCancelacion,
-    Moneda = m.Moneda,
-    TipoCambio  = m.TipoCambio,
-    ImporteTotal = calc.ImporteTotal
-                 * ISNULL(origenCont.Factor,1) 
-                 * eV.Factor,
-    FluctuacionCambiariaMN = 0,
-    ImporteTotalMN = ROUND(
-                            calc.ImporteTotal
-                          * m.TipoCambio
-                          * ISNULL(origenCont.Factor,1) 
-                          * eV.Factor
-                     , 4, 1),
+  IF @Tipo = 1 
+  BEGIN
+    INSERT INTO @EstatusValidos
+    ( 
+      Estatus,
+      EsCancelacion,
+      Factor 
+    )
+    VALUES 
+      ( 'CONCLUIDO', 0,  1 ),
+      ( 'PENDIENTE', 0,  1 ),
+      ( 'CANCELADO', 0,  1 ), 
+      ( 'CANCELADO', 1, -1 )
 
-    AuxiliarModulo = origenCont.AuxModulo,
-    AuxiliaMov = origenCont.AuxMov,
-    PolizaID =  pf.DID
-  FROM 
-    CUP_CxOrigenContable origenCont 
-  JOIN Compra m ON origenCont.Mov = m.Mov
-  JOIN Prov ON Prov.Proveedor = m.Proveedor
-  JOIN @EstatusValidos eV ON eV.Estatus = m.Estatus
-  -- CALC
-  CROSS APPLY(
-              SELECT
-                ImporteTotal = ISNULL(m.Importe,0) 
-                             + ISNULL(m.Impuestos,0)
-                             - ISNULL(m.Retencion,0)
-                             - ISNULL(m.DescuentoGlobal,0)
-             ) calc
-  -- Poliza Contable
-  OUTER APPLY( 
-               SELECT 
-                 mf.DID
-               FROM 
-                MovFlujo mf
-               WHERE 
-                 mf.DModulo = 'CONT'
-               AND mf.OModulo = origenCont.Modulo
-               AND mf.OID = m.ID               
-               AND ( 
-                      (   
-                          m.Estatus = 'CANCELADO'
-                      AND (
-                             (
-                                eV.EsCancelacion = 1 
-                             AND mf.DID = m.ContID
-                             )
-                          OR (
-                                eV.EsCancelacion = 0
-                             AND mf.DID < m.ContID
-                             )
-                          )
-                      )
-                  OR  (
-                        m.Estatus <> 'CANCELADO'
-                      AND mf.DID = m.ContID
-                      )
-                  )
-              ) pf
-  WHERE
-    OrigenCont.UsarAuxiliarNeto = 0
-  AND origenCont.Modulo = 'COMS'
-  AND (  origenCont.ValidarOrigen = 0
-      OR (  
-            origenCont.ValidarOrigen = 1 
-          AND ISNULL(origenCont.OrigenTipo,'') = ISNULL(m.OrigenTipo,'')
-          AND ISNULL(origenCont.Origen,'') = ISNULL(m.Origen,'')
+    SELECT
+      @Empleado,
+      origenCont.Modulo,
+      m.ID,
+      m.Mov,
+      m.MovId,
+      m.Sucursal,
+      m.FechaEmision,
+      m.Proveedor,
+      ProvNombre =  REPLACE(REPLACE(REPLACE(Prov.Nombre,CHAR(13),''),CHAR(10),''),CHAR(9),''),
+      ProvCuenta =  Prov.Cuenta,
+      m.Estatus,
+      eV.EsCancelacion,
+      Moneda = m.Moneda,
+      TipoCambio  = m.TipoCambio,
+      ImporteTotal = calc.ImporteTotal
+                    * ISNULL(origenCont.Factor,1) 
+                    * eV.Factor,
+      FluctuacionCambiariaMN = 0,
+      ImporteTotalMN = ROUND(
+                              calc.ImporteTotal
+                            * m.TipoCambio
+                            * ISNULL(origenCont.Factor,1) 
+                            * eV.Factor
+                        , 4, 1),
+
+      AuxiliarModulo = origenCont.AuxModulo,
+      AuxiliaMov = origenCont.AuxMov,
+      PolizaID =  pf.DID
+    FROM 
+      CUP_CxOrigenContable origenCont 
+    JOIN Compra m ON origenCont.Mov = m.Mov
+    JOIN Prov ON Prov.Proveedor = m.Proveedor
+    JOIN @EstatusValidos eV ON eV.Estatus = m.Estatus
+    -- CALC
+    CROSS APPLY(
+                SELECT
+                  ImporteTotal = ISNULL(m.Importe,0) 
+                                + ISNULL(m.Impuestos,0)
+                                - ISNULL(m.Retencion,0)
+                                - ISNULL(m.DescuentoGlobal,0)
+                ) calc
+    -- Poliza Contable
+    OUTER APPLY( 
+                  SELECT 
+                    mf.DID
+                  FROM 
+                  MovFlujo mf
+                  WHERE 
+                    mf.DModulo = 'CONT'
+                  AND mf.OModulo = origenCont.Modulo
+                  AND mf.OID = m.ID               
+                  AND ( 
+                        (   
+                            m.Estatus = 'CANCELADO'
+                        AND (
+                                (
+                                  eV.EsCancelacion = 1 
+                                AND mf.DID = m.ContID
+                                )
+                            OR (
+                                  eV.EsCancelacion = 0
+                                AND mf.DID < m.ContID
+                                )
+                            )
+                        )
+                    OR  (
+                          m.Estatus <> 'CANCELADO'
+                        AND mf.DID = m.ContID
+                        )
+                    )
+                ) pf
+    WHERE
+      OrigenCont.UsarAuxiliarNeto = 0
+    AND origenCont.Modulo = 'COMS'
+    AND (  origenCont.ValidarOrigen = 0
+        OR (  
+              origenCont.ValidarOrigen = 1 
+            AND ISNULL(origenCont.OrigenTipo,'') = ISNULL(m.OrigenTipo,'')
+            AND ISNULL(origenCont.Origen,'') = ISNULL(m.Origen,'')
+          )
         )
-      )
-  AND m.Ejercicio = @Ejercicio
-  AND m.Periodo = @Periodo
-  AND m.Estatus IN ('PENDIENTE','CANCELADO','CONCLUIDO')
+    AND m.Ejercicio = @Ejercicio
+    AND m.Periodo = @Periodo
+    AND m.Estatus IN ('PENDIENTE','CANCELADO','CONCLUIDO')
+  END
 END
