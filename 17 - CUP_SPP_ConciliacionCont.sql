f58fa3c18790620c558a222946369e5ed1ac026d
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-11-04 17:22:11 -0600

    Añade parametros predeterminados al sp principal de la caratula, junto con un simple criterio para validar que no se ejecute el proceso en caso que uno de los parametros sea NULO

--- 17 - CUP_SPP_ConciliacionCont.sql
+++ 17 - CUP_SPP_ConciliacionCont.sql
@@ -20,47 +20,53 @@ GO
 -- toda la informacion necesaria para realizar 
 -- las conciliaciones contable.
 --
--- Example: EXEC CUP_SPP_ConciliacionCont 63527, 1, 2016, 9
+-- Example: EXEC CUP_SPP_ConciliacionCont 63527, 1, 2016, 10
 -- =============================================
 
 
 CREATE PROCEDURE dbo.CUP_SPP_ConciliacionCont
-  @Empleado INT,
-  @Tipo INT,
-  @Ejercicio INT,
-  @Periodo INT
+  @Empleado INT = NULL,
+  @Tipo INT  = NULL,
+  @Ejercicio INT  = NULL,
+  @Periodo INT  = NULL
 AS BEGIN 
 
   SET NOCOUNT ON;
 
-  DELETE CUP_ConciliacionCont
-  WHERE Empleado = @Empleado
+  IF @Empleado IS NOT NULL
+  AND @Tipo IS NOT NULL
+  AND @Ejercicio IS NOT NULl
+  AND @Periodo IS NOT NULl
+  BEGIN
+
+    DELETE CUP_ConciliacionCont
+    WHERE Empleado = @Empleado
  
-  INSERT INTO CUP_ConciliacionCont
-  (
-    Empleado,
-    Tipo,
-    Ejercicio,
-    Periodo
-  )
-  VALUES
-  (
-    @Empleado,
-    @Tipo,
-    @Ejercicio,
-    @Periodo
-  )
+    INSERT INTO CUP_ConciliacionCont
+    (
+      Empleado,
+      Tipo,
+      Ejercicio,
+      Periodo
+    )
+    VALUES
+    (
+      @Empleado,
+      @Tipo,
+      @Ejercicio,
+      @Periodo
+    )
 
-  -- Auxiliares Cx
-  EXEC CUP_SPI_ConciliacionCont_AuxCx @Empleado, @Tipo, @Ejercicio, @Periodo
+    -- Auxiliares Cx
+    EXEC CUP_SPI_ConciliacionCont_AuxCx @Empleado, @Tipo, @Ejercicio, @Periodo
 
-  -- Auxiliares Contables
-  EXEC CUP_SPI_ConciliacionCont_AuxCont @Empleado, @Tipo, @Ejercicio, @Periodo
+    -- Auxiliares Contables
+    EXEC CUP_SPI_ConciliacionCont_AuxCont @Empleado, @Tipo, @Ejercicio, @Periodo
   
-  -- Auxiliares Modulo
-  EXEC CUP_SPI_ConciliacionCont_AuxModulo @Empleado, @Tipo, @Ejercicio, @Periodo
+    -- Auxiliares Modulo
+    EXEC CUP_SPI_ConciliacionCont_AuxModulo @Empleado, @Tipo, @Ejercicio, @Periodo
 
-  -- Devuelve la Caratula Contable de la conciliacion requerida
-  EXEC CUP_SPQ_ConciliacionCont_Caratula @Empleado, @Tipo, @Ejercicio, @Periodo
-
+    -- Devuelve la Caratula Contable de la conciliacion requerida
+    EXEC CUP_SPQ_ConciliacionCont_Caratula @Empleado, @Tipo, @Ejercicio, @Periodo
+  END
 END
