a231b150e1da658ad02dc0e52a1bb027f3dddbc9
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-11-18 17:03:44 -0600

    Actualiza las descripciones del formato de liberacion

--- SQL/Tables/CUP_ConciliacionCont.sql
+++ SQL/Tables/CUP_ConciliacionCont.sql
@@ -5,17 +5,16 @@ IF OBJECT_ID('dbo.CUP_ConciliacionCont', 'U') IS NOT NULL
 
 GO
 
--- =============================================
--- Created by:    Enrique Sierra Gtez
--- Creation Date: 2016-10-26
---
--- Description: Tabla encargada de contener
--- guardar la relacion de el empleado que esta 
--- consultando la herramienta de conciliacion 
--- contable, junto con el tipo de consulta 
--- que esta realizando.
---
--- =============================================
+ /* =============================================
+ Created by:    Enrique Sierra Gtez
+ Creation Date: 2016-10-26
+
+ Description: Tabla encargada de contener
+ la relacion de el empleado que esta 
+ consultando la conciliacion junto con el tipo de 
+ la misma.
+
+ ============================================= */
 
 CREATE TABLE dbo.CUP_ConciliacionCont
 (
