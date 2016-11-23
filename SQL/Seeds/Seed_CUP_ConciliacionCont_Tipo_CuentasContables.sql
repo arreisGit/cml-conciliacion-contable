5c35dea643874f9e5c9e273768d83b41b2c7e2ac
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-11-23 10:39:17 -0600

    Añade cuenta de proveedores aduanales a conciliacion saldos prov

--- SQL/Seeds/Seed_CUP_ConciliacionCont_Tipo_CuentasContables.sql
+++ SQL/Seeds/Seed_CUP_ConciliacionCont_Tipo_CuentasContables.sql
@@ -8,18 +8,19 @@ INSERT INTO
 )
 VALUES
   /* Saldo Proveedores */
-  ( 1, '211-100-000-0000'), -- Proveedores Nacionales	
-  ( 1, '211-200-000-0000'), -- Proveedores de Importacion	
-  ( 1, '211-500-000-0000'), -- Proveedores Interempresas( 1, No Usar)	
-  ( 1, '211-600-001-0000'), -- Alutodo SA de CV	
-  ( 1, '211-600-002-0000'), -- Metales Diaz SA de CV   	
-  ( 1, '211-600-003-0000'), -- Servicios Cuprum SA de CV	
-  ( 1, '211-600-004-0000'), -- Cuprum SA de CV
-  ( 1, '211-600-005-0000'), -- Tiendas Cuprum SA de CV	
-  ( 1, '211-600-006-0000'), -- Grupo Cuprum SA de CV   	
-  ( 1, '211-600-009-0000')  -- Cuprum Fab SA de CV	
+   ( 1, '211-100-000-0000') -- Proveedores Nacionales	
+  ,( 1, '211-200-000-0000') -- Proveedores de Importacion	
+  ,( 1, '211-500-000-0000') -- Proveedores Interempresas( 1, No Usar)	
+  ,( 1, '211-600-001-0000') -- Alutodo SA de CV	
+  ,( 1, '211-600-002-0000') -- Metales Diaz SA de CV   	
+  ,( 1, '211-600-003-0000') -- Servicios Cuprum SA de CV	
+  ,( 1, '211-600-004-0000') -- Cuprum SA de CV
+  ,( 1, '211-600-005-0000') -- Tiendas Cuprum SA de CV	
+  ,( 1, '211-600-006-0000') -- Grupo Cuprum SA de CV   	
+  ,( 1, '211-600-009-0000') -- Cuprum Fab SA de CV	
+  ,( 1, '211-400-001-0000') -- Proveedores Agentes Aduanales
   /* Iva Por Acreditar */
-  ,( 2, '119-210-000-0000') -- IVA 16% Por Acreditar,
+  ,( 2, '119-210-000-0000') -- IVA 16% Por Acreditar
   /* Saldo Clientes */
   ,( 3, '113-100-000-0000') -- Clientes GDL
   ,( 3, '113-200-000-0000') -- Clientes MEX
