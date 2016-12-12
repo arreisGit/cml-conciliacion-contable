7a1445f7ae7e1c46d6b268a1d657d58132f66461
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-12-12 12:29:26 -0600

    Añade vista para obtener el listado de todos los depositos que provienen de cortes caja y asi poder utilizarlo en la conciliacion del IVA Trasladado

---
+++ SQL/Views/CUP_v_DepositosCortesCaja.sql
@@ -0,0 +1,66 @@
+    SET ANSI_NULLS, ANSI_WARNINGS ON;
+
+GO 
+
+/*=============================================
+ Created by:    Enrique Sierra Gtez
+ Creation Date: 2016-12-12
+
+ Description: Regresa un listado de los
+ depositos que aplicaron a solicitudes
+ de deposito disparadas por cortes de caja
+ ( chica o tombola ).
+
+ Example: SELECT * 
+          FROM  CUP_v_DepositosCortesCaja
+          WHERE 
+            Ejercicio = 2016
+          AND Periodo = 10
+            
+-- ============================================*/
+
+
+IF EXISTS(SELECT * FROM sysobjects WHERE name='CUP_v_DepositosCortesCaja')
+	DROP VIEW CUP_v_DepositosCortesCaja
+GO
+CREATE VIEW CUP_v_DepositosCortesCaja
+AS    
+SELECT DISTINCT
+  dep.ID,
+  dep.Mov,
+  dep.Movid,
+  dep.FechaEmision,
+  dep.Ejercicio,
+  dep.Periodo,
+  dep.Estatus,
+  dep.CtaDinero,
+  dep.CtaDineroDestino,
+  depD.Aplica,
+  depD.AplicaID,
+  depD.Importe,
+  depD.FormaPago,
+  solDev.IVAFiscal
+FROM
+Dinero dep 
+JOIN DineroD depD ON depD.ID = dep.ID
+JOIN Movtipo t ON t.Modulo = 'DIN'
+            AND t.Mov = dep.Mov
+JOIN Movtipo aplicaT ON aplicaT.Modulo = 'DIN'
+                  AND aplicaT.Mov = depD.Aplica
+JOIN dinero solDev ON solDev.Mov = depD.Aplica
+                AND solDev.MovID = depD.AplicaID
+-- Corte Caja / Corte Tombola.
+CROSS APPLY(SELECT TOP 1 
+            ID = mf.OID 
+          FROM 
+            dbo.fnCMLMovFlujo('DIN', solDev.ID, 1) mf 
+          WHERE 
+            mf.Indice < 0 
+          AND mf.OModulo = 'DIN'
+          AND mf.OMovTipo = 'DIN.CP'
+          AND mf.OMovSubTipo = 'DIN.CPMULTIMONEDA') corte
+WHERE 
+t.Clave = 'DIN.D'
+AND aplicaT.Clave = 'DIN.SD'
+AND dep.Estatus IN ( 'CONCLUIDO', 'CANCELADO' )
+AND corte.ID IS NOT NULL
