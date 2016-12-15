9a6529bf12ed864d6ba5c4b4f9592630aa0e5d41
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2016-12-15 17:36:13 -0600

    Añade validacion en los indices de dineroD para solo ser creados si no existen

--- SQL/Missing Index/DineroD.sql
+++ SQL/Missing Index/DineroD.sql
@@ -1,3 +1,14 @@
-CREATE NONCLUSTERED INDEX [IX_DineroD_Aplica_AplicaID]
-ON [dbo].[DineroD] ([Aplica],[AplicaID])
-INCLUDE ([ID],[Importe],[FormaPago])
+IF NOT EXISTS(SELECT 
+                * 
+              FROM
+                sys.indexes
+              WHERE
+                name = 'IX_DineroD_Aplica_AplicaID'
+              AND object_id = OBJECT_ID('DineroD'))
+BEGIN
+
+  CREATE NONCLUSTERED INDEX [IX_DineroD_Aplica_AplicaID]
+  ON [dbo].[DineroD] ([Aplica],[AplicaID])
+  INCLUDE ([ID],[Importe],[FormaPago])
+
+END
